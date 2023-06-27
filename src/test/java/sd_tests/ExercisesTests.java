4b22e4d20e5698670e9014a71d17c9ebf23f9244
Svetlanushka1 <112639604+Svetlanushka1@users.noreply.github.com>
2023-06-27 10:35:27 +0300

    Update ExercisesTests.java

--- src/test/java/sd_tests/ExercisesTests.java
+++ src/test/java/sd_tests/ExercisesTests.java
@@ -21,17 +21,6 @@ public class ExercisesTests extends TestBase {
 
     @Test
     clickOnExerciesIcon();
-    clickOnTabs();
-    clickOnChords()
-        selectExercisesCard();
-    //list of cards  = //div[@class='exercise-card-container trim-text']
-    clickOnButtonStartSixNineChord();
-//body[1]/div[1]/div[1]/div[2]/main[1]/div[2]/div[2]/div[1]/div[2]/div[3]/div[1]/div[1]/div[2]/div[3]/button[1]
-    clickOnBtnStartSetup();
-
-    //div[@class='text-center q-pb-md']//button[@class='app-btn primary medium text-black']
-    clickOnBtnSartLast();
-
-    //button[@class='app-btn primary medium text-black']
+   
 
 }
