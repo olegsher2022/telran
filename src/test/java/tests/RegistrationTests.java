50f2bd4b5ee533bcc10965334cb83bbbc0d3070c
Oleg Sher <olegsher@revuze.it>
2023-11-23 11:39:57 +0200

    lesson 13 added methods to negative registration RegistrationTests

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -20,4 +20,46 @@ public class RegistrationTests extends BaseTest{
         app.getUserHelper().fillRegistrationForm(user);
         Assert.assertTrue(app.getUserHelper().validatePopUpMessageSuccessAfterRegistration());
     }
+
+    //NEGATIVE REGISTRATION
+    @Test
+    public void negativeRegistrationWrongEmail() {
+        UserDTOLombok user = UserDTOLombok.builder()
+                .email("abc@")
+                .password("123456Aa$")
+                .lastName("abdfg")
+                .name("test")
+                .build();
+
+        app.getUserHelper().fillRegistrationForm(user);
+        Assert.assertTrue(app.getUserHelper().validateMessageIncorrectEmailReg());
+    }
+
+    @Test
+    public void negativeRegistrationWrongPassword() {
+        String email = randomUtils.generateEmail(7);
+
+        UserDTOLombok user = UserDTOLombok.builder()
+                .email(email)
+                .password("123456Aa")
+                .lastName("abdfg")
+                .name("test")
+                .build();
+
+        app.getUserHelper().fillRegistrationForm(user);
+        Assert.assertTrue(app.getUserHelper().validateMessageWrongPasswordReg());
+    }
+
+    @Test
+    public void negativeRegistrationBlankEmail() {
+        UserDTOLombok user = UserDTOLombok.builder()
+                .email("")
+                .password("123456Aa$")
+                .lastName("abdfg")
+                .name("test")
+                .build();
+
+        app.getUserHelper().fillRegistrationForm(user);
+        Assert.assertTrue(app.getUserHelper().validateErrorEmptyEmailReg());
+    }
 }
